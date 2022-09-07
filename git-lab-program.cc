8b38a460432cc06e4a8b2873c616c2dcb8dd07a2
Josh Khalil <jk115720@ohio.edu>
2022-09-07 18:02:17 -0400

    Final Program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Josh Khalil
+ *        Date: 9/7/2022
+ * Description: Displays a message on the screen.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" ;
+    return 0;
+}// main
